e3e82f508332e0fb5d3ebee3697523552d153e41
Devlin @ Eduworks <djunker@Devlins-MacBook-Air.local>
2014-08-23 14:32:23 -0700

    finished caching inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching Matrix Mean
+## -------------------
+## Functions for matrix container that caches the inverse of the matrix
+## for performance reasons
 
-## Write a short comment describing this function
+## Creates and returns the matrix container that caches the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  
+  get <- function(){
+    x
+  }
+  
+  set <- function(y = matrix()){
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  getInverse <- function(){
+    inverse
+  }
+  
+  setInverse <- function(i = NULL){
+    inverse <<- i
+  }
+  
+  list(get = get, set = set, 
+        getInverse= getInverse, setInverse = setInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse()
+        if(!is.null(i)){
+          return(i)
+        }
+        
+        matrix <- x$get()
+        i <- solve(matrix, ...)
+        x$setInverse(i)
+        i
 }
